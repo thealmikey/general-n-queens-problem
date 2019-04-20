f70d18bce9eee97cda6b467c0acffde2a7212bb7
thealmikey <thealmikey@gmail.com>
2019-04-20 11:17:24 +0300

    Added attackingPositions method to Chesspiece trait
    The attackingPositions method returns a list of positions under attack by piece once we place it on the board it takes in the board as input and uses it's position in a closure as well and return all affected position where other pieces can't occupy.

--- src/main/scala/com/almikey/chesschallenge/ChessPieces.scala
+++ src/main/scala/com/almikey/chesschallenge/ChessPieces.scala
@@ -1,5 +1,7 @@
 package com.almikey.chesschallenge
 
+import com.almikey.chesschallenge.ChessBoard.ChessBoard
+
 object ChessPieces {
 
   type PiecePosition = (Int, Int)
@@ -7,8 +9,11 @@ object ChessPieces {
   sealed trait ChessPiece {
     def position: PiecePosition
     def canCaptureOther(chessPiece: ChessPiece): Boolean
+    def attackingPositions(board: ChessBoard): List[(Int, Int)]
   }
   case class Blank(var position: PiecePosition = (0, 0)) extends ChessPiece {
     override def canCaptureOther(chessPiece: ChessPiece): Boolean = false
+
+    override def attackingPositions(board: ChessBoard): List[(Int, Int)] = Nil
   }
 }
