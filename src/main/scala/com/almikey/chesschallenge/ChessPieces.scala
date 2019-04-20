259a421ac07d8a9cc4c035323654e8b89a6dd926
thealmikey <thealmikey@gmail.com>
2019-04-20 09:55:36 +0300

    Added a ChessBoard object to generate board of various size
    Added a ChessBoard object containing generateBoard(N,M) method which generates a Vector with a tuple of (position:(int,int),piece) which by default contains Blank chess piece to represent empty and tests along to ensure size and blank pieces

--- src/main/scala/com/almikey/chesschallenge/ChessPieces.scala
+++ src/main/scala/com/almikey/chesschallenge/ChessPieces.scala
@@ -8,7 +8,7 @@ object ChessPieces {
     def position: PiecePosition
     def canCaptureOther(chessPiece: ChessPiece): Boolean
   }
-  case class Blank(var position: PiecePosition) extends ChessPiece {
+  case class Blank(var position: PiecePosition = (0, 0)) extends ChessPiece {
     override def canCaptureOther(chessPiece: ChessPiece): Boolean = false
   }
 }
---
+++ src/test/scala/com/almikey/chesschallenge/ChessBoardTest.scala
@@ -0,0 +1,18 @@
+package com.almikey.chesschallenge
+
+import com.almikey.chesschallenge.ChessPieces.{Blank, ChessPiece}
+import org.scalatest.{FlatSpec, Matchers}
+
+class ChessBoardTest extends FlatSpec with Matchers {
+  "generate 2 by 2 chessboard" should "return a vector of size 4" in {
+    var newBoard = ChessBoard.generateBoard(2, 2)
+    assert(newBoard.isInstanceOf[Vector[(Int, Int, ChessPiece)]])
+    assert(newBoard.size == 4)
+  }
+  "new chess boards" should "have Blank() chess pieces" in {
+    var newBoard = ChessBoard.generateBoard(3, 4)
+    newBoard.foreach { x =>
+      assert(x._3.isInstanceOf[Blank])
+    }
+  }
+}
